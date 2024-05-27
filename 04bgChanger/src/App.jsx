692064a822c60dc5c4c70218a14e60994c9025fa
pratik-kharva27 <pratik01proses@gmail.com>
2024-05-27 23:48:34 +0530

    04 bg project complete

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -11,7 +11,13 @@ const [color,setColor] = useState('gray')
     <div className='w-full h-screen duration-100'
     style={{backgroundColor: color}}
     >
-      <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2'> test</div>
+      <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2'> 
+      <div className='flex flex-wrap justify-center  gap-3 shadow-lg bg-black px-3 py-2 rounded-3xl'>
+      <button onClick={()=> setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'>Red</button> 
+      <button onClick={()=> setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'>Blue</button>
+      <button onClick={()=> setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'>yellow</button>
+      </div>
+      </div>
     </div>
     </>
   )
